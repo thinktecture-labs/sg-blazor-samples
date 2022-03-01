9474149e7c21f4242284f8f29f563613506f6079
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2022-03-01 10:40:10 +0100

    feat(app): add theme manager example

--- src/BlazorExamples/App.razor.cs
+++ src/BlazorExamples/App.razor.cs
@@ -1,19 +1,6 @@
-﻿using MudBlazor;
-using MudBlazor.Utilities;
-
-namespace BlazorExamples
+﻿namespace BlazorExamples
 {
 	public partial class App
 	{
-		private MudTheme _theme = new MudTheme()
-		{
-			Palette = new Palette()
-			{
-				Primary = new MudColor("#ff584f"),
-				Secondary = new MudColor("#3d6fb4"),
-				TextPrimary = new MudColor("#ffffff"),
-				TextSecondary = new MudColor("#ffffff"),
-			},
-		};
 	}
 }
--- src/BlazorExamples/Shared/MainLayout.razor.cs
+++ src/BlazorExamples/Shared/MainLayout.razor.cs
@@ -1,12 +1,43 @@
-﻿namespace BlazorExamples.Shared
+﻿using MudBlazor;
+using MudBlazor.ThemeManager;
+using MudBlazor.Utilities;
+
+namespace BlazorExamples.Shared
 {
 	public partial class MainLayout
 	{
+		private ThemeManagerTheme _themeManager = new ThemeManagerTheme()
+		{
+			Theme = new MudTheme()
+			{
+				Palette = new Palette()
+				{
+					Primary = new MudColor("#ff584f"),
+					Secondary = new MudColor("#3d6fb4"),
+					TextPrimary = new MudColor("#000000"),
+					TextSecondary = new MudColor("#ffffff"),
+					AppbarBackground = new MudColor("#ff584f"),
+				},
+			},
+		};
+
 		private bool _menuExpanded = true;
+		private bool _themeManagerExpanded = false;
 
 		private void ToggleMenu()
 		{
 			_menuExpanded = !_menuExpanded;
+		}
+
+		private void ToggleThemeManager()
+		{
+			_themeManagerExpanded = !_themeManagerExpanded;
+		}
+
+		private void UpdateTheme(ThemeManagerTheme theme)
+		{
+			_themeManager = theme;
+			StateHasChanged();
 		}
 	}
 }
