63930ac75e00e07865e7c1e5d643cce17838f96e
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2022-03-22 21:01:46 +0100

    feat(WASM): add custom bootup script

--- src/BlazorWasmHost/Program.cs
+++ src/BlazorWasmHost/Program.cs
@@ -3,6 +3,12 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.JSInterop;
 using SharedLibrary;
 
+
+Console.WriteLine("Program.main entry");
+
+await Task.Delay(1);
+Console.WriteLine("After first async call");
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -11,6 +17,7 @@ builder.Services.AddScoped(sp => new HttpClient() { BaseAddress = new Uri(builde
 
 builder.Services.AddSharedStuff();
 
+Console.WriteLine("Building host");
 var host = builder.Build();
 
 // tell JS to display the wait-text and wait a bit
@@ -19,4 +26,5 @@ var jsRuntime = host.Services.GetRequiredService<IJSRuntime>();
 await jsRuntime.InvokeVoidAsync("window.waitForStartup");
 await Task.Delay(TimeSpan.FromSeconds(3));
 
+Console.WriteLine("Starting host");
 await host.RunAsync();
